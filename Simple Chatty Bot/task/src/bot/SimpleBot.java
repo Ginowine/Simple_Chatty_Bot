18ec97ff590fbbbba11dd7f80abc2e08d0973c92
ginowine <ginowine1@gmail.com>
2020-09-10 15:05:01 +0100

    Initial commit

---
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,12 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        // write your code here
+
+        String name = "Gino";
+        int birthYear = 2020;
+        System.out.println("Hello! My name is " + name);
+        System.out.println("I was ceeated in" + birthYear);
+    }
+}
