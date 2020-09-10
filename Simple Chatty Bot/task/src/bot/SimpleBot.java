77ddfd5c501af483694bf0846a27e551c1a70d7e
ginowine <ginowine1@gmail.com>
2020-09-10 16:51:42 +0100

    program predicts age

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -6,17 +6,26 @@ public class SimpleBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String name = "Gino";
+        String botName = "Gino";
         int createdYear = 2020;
 
-        System.out.println("Hello! My name is " + name);
+        System.out.println("Hello! My name is " + botName );
         System.out.println("I was created in " + createdYear);
         System.out.println("Please, remind me your name.");
 
-        String userName = scanner.next();
+        String userName = scanner.nextLine();
 
-        // reading a name
+        System.out.println("What a great name you have, " + userName + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
-        System.out.println("What a great name you have !" + userName);
+        // reading all remainders
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.println("Your age is " + age + ";" + " that's a good time to start programming!");
     }
 }
